a35abaa830254fc449a3c15e213f4de3147b800c
FelipeFranke5 <frankefelipee@gmail.com>
2024-12-23 11:40:28 -0300

    Move UserRepository to a repository folder.

--- src/main/java/com/frankefelipee/myissuertracker/repository/UserRepository.java
+++ src/main/java/com/frankefelipee/myissuertracker/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package com.frankefelipee.myissuertracker.user;
+package com.frankefelipee.myissuertracker.repository;
 
+import com.frankefelipee.myissuertracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
